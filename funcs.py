909b21be0c445d78506f22222d6a0f303a8e50e8
patrickPetscavage <143347569+patrickPetscavage@users.noreply.github.com>
2024-05-02 09:51:03 -0400

    funcs.py
    funcs.py is a simple module where I put a few variables that would be changing throughout the code.

---
+++ funcs.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 16:41:24 2024
+
+@author: pjpet
+"""
+
+listOfSearchedGames = []
+gamesAdded = []
+amountOfGamesInCart = 0
+totalPrice = 0.0
+gameClicked = ""
+listOfLabels = []
